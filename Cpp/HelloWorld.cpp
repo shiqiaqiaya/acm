29d7287f014a34cc3db3558d01aa7b921442897e
shiqiaqiaya <1952318100@qq.com>
2024-05-19 08:52:50 +0800

    HelloWorld

---
+++ Cpp/HelloWorld.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void QWQ() {
+
+}
+
+void QAQ() {
+
+}
+
+signed main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+
+    QWQ();
+
+    while (t--) {
+        QAQ();
+    }
+}
